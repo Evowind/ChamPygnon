04c95905a324c3bebe1d843c09456b19cdbf0c11
Samy <samy.tadly@etu.u-pec.fr>
2024-04-12 22:08:48 +0200

    Mises a jour des com de prediction.py

--- prediction.py
+++ prediction.py
@@ -2,31 +2,31 @@ import sys
 import pickle
 
 def predict(model_path, features):
-    # Load the trained model
+    # Charger le modèle entraîné
     with open(model_path, 'rb') as model_file:
         model = pickle.load(model_file)
 
-    # Prepare the features for prediction
+    # Préparer les caractéristiques pour la prédiction
     redValue, blueValue, greenValue, shape, surface = features[:5]
-    # Convert categorical features to one-hot encoded format
+    # Convertir les caractéristiques catégorielles au format encodé one-hot
     shape_encoded = [1 if s == shape else 0 for s in ['Polypore', 'Convex', 'Bell', 'Depressed', 'CupFungi', 'CoralFungi', 'Conical', 'Flat', 'JellyFungi', 'Stinkhorns', 'Earthstars', 'Puffballs', 'Corticioid', 'Chanterelles', 'Funnel', 'Cylindrical', 'Knobbed', 'Shell', 'Truffles', 'Bolete', 'FalseMorels', 'ToothFungi', 'TrueMorels', 'Cauliflower']]
     surface_encoded = [1 if s == surface else 0 for s in ['Smooth', 'FlatScales', 'Fibrous', 'Patches', 'RaisedScales', 'Hairy', 'Powder', 'Silky', 'Velvety']]
 
-    # Combine all features
+    # Combinaison de toutes les caractéristiques
     feature_values = [float(redValue), float(blueValue), float(greenValue)] + shape_encoded + surface_encoded
 
-    # Make predictions
+    # Faire des prédictions
     predictions = model.predict([feature_values])
 
     return predictions[0]
 
 if __name__ == "__main__":
-    # Get the model path and features from command line arguments
+    # Obtenir le chemin du modèle et les caractéristiques à partir des arguments de la ligne de commande
     model_path = sys.argv[1]
     features = sys.argv[2:]
 
-    # Perform prediction
+    # Effectuer la prédiction
     result = predict(model_path, features)
 
-    # Print the result
+    # Afficher le résultat
     print(result)
